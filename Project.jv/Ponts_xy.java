dfd5066a56a2b01f7dbf59ab1ba88a2f8a0741ed
DanielStark <kamirates@gmail.com>
2022-01-05 18:22:17 -0300

    novo arquivo

---
+++ Project.jv/Ponts_xy.java
@@ -0,0 +1,24 @@
+
+
+public class Ponts_xy{
+
+public static void main( String [] args){
+
+    Scanner input = new Scanner(System.in);
+    private int x,y;
+
+    System.out.println("Digite a cordenada do ponto (x):");
+    int x = input.nextInt();
+    System.out.println("Digite a cordenada do ponto (y):");
+    int y = input.nextInt();
+
+} 
+
+public sattic int cordenadas( int x,int y){
+
+    for(int x = 0,int y = 0; x < 0, y < 0; ++1){
+        System.out.println(x,y)
+    }
+}
+
+}
