855e0fd9e56160cccee8d407fd7789257080483f
Daniel Gomes dos Santos <67829982+Danielstarks@users.noreply.github.com>
2022-01-05 17:53:24 -0300

    Add files via upload

---
+++ projectLoiane/src/main/java/aulasBasicos/DadosPrimitivo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulasBasicos;
+
+/**
+ *
+ * @author danielstark
+ */
+public class DadosPrimitivo {
+
+    public static void main(String[] args) {
+
+        /* Tipos primitivos
+        
+         Inteiros: byte, short, int, long, char.
+         Pontos flutuantes: float e double.
+         Boolean: true ou false.  
+        
+         */
+        
+        byte a = 10; short b = 20; int  c = 30; long d = 40;
+        float e = 3.4f; double f = 5.4;
+        System.out.println("valor de inteiros: " + a+b+c+d);
+        System.out.println("valor de pontos flutuantes: " +e+" " +f);   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+}
