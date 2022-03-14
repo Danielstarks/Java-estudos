ef5a538ab6497044db5a8b2f15bf76f63cc5f534
DanielStark <kamirates@gmail.com>
2022-03-14 13:29:54 -0300

    ok

---
+++ curso_js.java/aulas_02 copy.java
@@ -0,0 +1,15 @@
+
+package curso_js.java;
+
+public class aulas_02 {
+    public static void main(String[] args) {
+        
+        int x;
+        x = 24;
+        
+        
+        
+        System.out.println("Você tem " + x + " anos");
+    }
+    
+}
---
+++ curso_js.java/aulas_13.java
@@ -0,0 +1,9 @@
+package curso_js.java;
+
+public class aulas_13 {
+
+    public static void main(String[] args) {
+
+        System.out.println("peixes de daniel ");
+    }
+}
