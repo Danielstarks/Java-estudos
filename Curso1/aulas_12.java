68f5e5256c65c53769ed4a8bedbeb350f8c98f72
DanielStark <kamirates@gmail.com>
2022-03-14 13:48:58 -0300

    atualização de pastas

--- Curso1/aulas_12.java
+++ Curso1/aulas_12.java
@@ -6,7 +6,7 @@ public class aulas_12{
     public static void main (String[] args){
 
 
-        System.out.println("");
+        System.out.println("ooçoç");
     }
 }
 
--- Curso1/aulas_13.java
+++ Curso1/aulas_13.java
@@ -4,6 +4,6 @@ public class aulas_13 {
 
     public static void main(String[] args) {
 
-        System.out.println("peixes de daniel ");
+        System.out.println("peixes ");
     }
 }
